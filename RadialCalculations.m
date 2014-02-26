f3304430771f5fbc369e16c9058f221f3bd8b9e8
unknown <tjs1g10@F9.ecs.soton.ac.uk>
2014-02-26 13:24:54 +0000

    Added matlab files for radial calculations

---
+++ RadialCalculations.m
@@ -0,0 +1,24 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% ELEC6089 High Volatage Insulation Design - Bushing Design
+%% Calculates the lengths of each foil layer - Radial grading
+%% Author - TJS and BH
+%% date - 26/02/2014
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+N = 21               %Number of foils
+length = zeros(N, 1) %initiate vectors
+radius = zeros(N, 1)
+radius(1) = 52       %set inner radius
+length(1) = 5000     %first foil length
+radius(21) = 148     %outer foil radius
+spacing = (radius(21)-radius(1))/(N-1) %even spacing of foils
+
+%calculate each foil radius and length - from Kuffel and Lecture Notes
+for i=2:N
+    radius(i) = radius(i-1)+spacing
+    length(i) = (length(i-1)*radius(i-1))/radius(i)
+end
+
+%Plot a graph to indicate validity
+lengthhalved = length/2 %half lengths to index from center
+bar(radius, lengthhalved) %plot bars with radius
