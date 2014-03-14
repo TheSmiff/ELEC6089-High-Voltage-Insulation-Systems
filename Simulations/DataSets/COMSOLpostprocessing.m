2796ee5b650ccca18760e8135e312d89dc77ff1d
TheSmiff <tjs1g10@ecs.soton.ac.uk>
2014-03-14 12:49:57 +0000

    More complete on post processing
    Signed-off-by: TheSmiff <tjs1g10@ecs.soton.ac.uk>

--- Simulations/DataSets/COMSOLpostprocessing.m
+++ Simulations/DataSets/COMSOLpostprocessing.m
@@ -11,4 +11,16 @@ No_Grad_Wall_R = data(:, 1);
 No_Grad_Wall_Z = data(:, 2);
 No_Grad_Wall_E_Norm = data(:, 3);
 
-plot(No_Foil_Wall_R, No_Foil_Wall_E_Norm, No_Grad_Wall_R, No_Grad_Wall_E_Norm);
+COMSOL_File_Import('Axial_Wall.txt')
+data = sortrows(data, 1);
+Axial_Wall_R = data(:, 1);
+Axial_Wall_Z = data(:, 2);
+Axial_Wall_E_Norm = data(:, 3);
+
+COMSOL_File_Import('Radial_Wall.txt')
+data = sortrows(data, 1);
+Radial_Wall_R = data(:, 1);
+Radial_Wall_Z = data(:, 2);
+Radial_Wall_E_Norm = data(:, 3);
+
+plot(No_Foil_Wall_R, No_Foil_Wall_E_Norm, No_Grad_Wall_R, No_Grad_Wall_E_Norm, Axial_Wall_R, Axial_Wall_E_Norm, Radial_Wall_R, Radial_Wall_E_Norm);
