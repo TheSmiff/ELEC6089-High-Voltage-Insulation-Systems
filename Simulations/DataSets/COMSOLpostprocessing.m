207479afc1ecaf9d458712781a095fb674f35212
TheSmiff <tjs1g10@ecs.soton.ac.uk>
2014-03-19 13:42:38 +0000

    Edited Figures
    Signed-off-by: TheSmiff <tjs1g10@ecs.soton.ac.uk>

--- Simulations/DataSets/COMSOLpostprocessing.m
+++ Simulations/DataSets/COMSOLpostprocessing.m
@@ -24,3 +24,4 @@ Radial_Wall_Z = data(:, 2);
 Radial_Wall_E_Norm = data(:, 3);
 
 plot(No_Foil_Wall_R, No_Foil_Wall_E_Norm, No_Grad_Wall_R, No_Grad_Wall_E_Norm, Axial_Wall_R, Axial_Wall_E_Norm, Radial_Wall_R, Radial_Wall_E_Norm);
+axes[0 200 0 5000000];
